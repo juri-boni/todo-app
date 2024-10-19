4a2fcab9a9b5015944944735231484c447f62340
juri-boni <100702839+juri-boni@users.noreply.github.com>
2024-10-19 11:25:43 +0200

    Added Error handling middleware in index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
 import path from "path";
 
@@ -12,6 +12,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use("/api", todosRoutes);
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Something went wrong!" });
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is running!");
